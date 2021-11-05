1fe9807648b928384b28102528b3b12a9d79be88
Bruce Anderson <anderson.bruce.b@gmail.com>
2021-11-05 16:43:12 -0400

    ...

--- be-reflective.js
+++ be-reflective.js
@@ -1,76 +1,114 @@
-import { XtalDecor } from 'xtal-decor/xtal-decor.js';
-import { CE } from 'trans-render/lib/CE.js';
-function doPoll(lastValue, key, self, prop) {
-    if (lastValue !== self[key]) {
-        lastValue = self[key];
-        if (lastValue !== undefined && lastValue != null) {
-            const sLV = lastValue.toString();
-            if (prop.as !== undefined) {
-                self.setAttribute(prop.as, sLV);
+import { define } from 'be-decorated/be-decorated.js';
+import { register } from "be-hive/register.js";
+export class BeReflectiveController {
+    #target;
+    intro(proxy, target, beDecorProps) {
+        this.#target = target;
+        const params = JSON.parse(proxy.getAttribute('is-' + beDecorProps.ifWantsToBe));
+        const fullyQualifiedMap = {};
+        for (const propKey in params) {
+            const propMap = params[propKey];
+            switch (typeof propMap) {
+                case 'string':
+                    const reflectTo = propMap;
+                    fullyQualifiedMap[propKey] = { reflectTo };
+                    break;
+                case 'object':
+                    fullyQualifiedMap[propKey] = propMap;
+                    break;
+                default:
+                    throw 'NI'; //Not implemented
+            }
+        }
+        proxy.map = fullyQualifiedMap;
+    }
+    onMap({ map }) {
+        for (const propKey in map) {
+            const propMap = map[propKey];
+            const { reflectTo, maxDelay, once } = propMap;
+            const val = this.#target[propKey];
+            if (val !== undefined) {
+                this.setAttr(this.#target, val, reflectTo);
+                if (once)
+                    return;
+            }
+            if (maxDelay) {
+                this.doPoll(propMap, propKey);
             }
             else {
-                self.dataset[key] = sLV;
+                let proto = this;
+                let prop = Object.getOwnPropertyDescriptor(proto, propKey);
+                while (proto && !prop) {
+                    proto = Object.getPrototypeOf(proto);
+                    prop = Object.getOwnPropertyDescriptor(proto, propKey);
+                }
+                if (prop === undefined) {
+                    throw { element: this, propKey, message: "Can't find property." };
+                }
+                const setter = prop.set.bind(this);
+                const getter = prop.get.bind(this);
+                Object.defineProperty(this, propKey, {
+                    get() {
+                        return getter();
+                    },
+                    set(nv) {
+                        setter(nv);
+                        this.setAttr(this.#target, nv, reflectTo);
+                    },
+                    enumerable: true,
+                    configurable: true,
+                });
             }
         }
     }
-    setTimeout(() => {
-        doPoll(lastValue, key, self, prop);
-    }, prop.poll);
+    doPoll(propMap, propKey) {
+        const { maxDelay, once, reflectTo } = propMap;
+        setTimeout(() => {
+            const val = this.#target[propKey];
+            if (val !== undefined) {
+                this.setAttr(this.#target, val, reflectTo);
+                if (once)
+                    return;
+            }
+            this.doPoll(propMap, propKey);
+        }, maxDelay);
+    }
+    setAttr(self, val, reflectTo) {
+        if (reflectTo[0] === '.') {
+            const verb = val ? 'add' : 'remove';
+            self.classList[verb](reflectTo.slice(1));
+        }
+        else if (reflectTo.substr(0, 2) === '::') {
+            const verb = val ? 'add' : 'remove';
+            self.part[verb](reflectTo.slice(2));
+        }
+        else {
+            const verb = val ? 'setAttribute' : 'removeAttribute';
+            self[verb](reflectTo, '');
+        }
+    }
 }
-const ce = new CE({
+const tagName = 'be-reflective';
+const ifWantsToBe = 'reflective';
+const upgrade = '*';
+define({
     config: {
-        tagName: 'be-reflective',
+        tagName,
         propDefaults: {
-            upgrade: '*',
-            ifWantsToBe: 'reflective',
-            props: {
-                src: {}
-            },
-            virtualProps: [],
+            virtualProps: ['map'],
+            noParse: true,
+            upgrade,
+            ifWantsToBe,
+            intro: 'intro',
+        },
+        actions: {
+            onMap: {
+                ifAllOf: ['map'],
+            }
         }
     },
     complexPropDefaults: {
-        actions: [
-            ({ self, props, target }) => {
-                for (const key in props) {
-                    const prop = props[key];
-                    if (prop.poll) {
-                        let lastValue = undefined;
-                        doPoll(lastValue, key, self, prop);
-                    }
-                    else {
-                        const proto = target.constructor.prototype;
-                        const newProp = Object.getOwnPropertyDescriptor(proto, key);
-                        const setter = newProp.set;
-                        const getter = newProp.get;
-                        Object.defineProperty(target, key, {
-                            get() {
-                                const tempGetter = getter.bind(this);
-                                return tempGetter();
-                            },
-                            set(nv) {
-                                if (nv !== null && nv !== undefined) {
-                                    const sNV = nv.toString();
-                                    if (prop.as !== undefined) {
-                                        self.setAttribute(prop.as, sNV);
-                                    }
-                                    self.dataset[key] = nv.toString();
-                                }
-                                const tempSetter = setter.bind(this);
-                                tempSetter(nv);
-                            },
-                            enumerable: true,
-                            configurable: true,
-                        });
-                    }
-                }
-            }
-        ],
-        init: (self) => {
-            console.log('in init');
-        },
-        on: {},
-    },
-    superclass: XtalDecor,
+        controller: BeReflectiveController,
+    }
 });
-document.head.appendChild(document.createElement('be-reflective'));
+register(ifWantsToBe, upgrade, tagName);
--- be-reflective.ts
+++ be-reflective.ts
@@ -1,81 +1,122 @@
-import {XtalDecor, XtalDecorCore} from 'xtal-decor/xtal-decor.js';
-import {CE} from 'trans-render/lib/CE.js';
-import {BeReflectiveProps, PropMonitor} from './types';
+import {define, BeDecoratedProps} from 'be-decorated/be-decorated.js';
+import {BeReflectiveProps, BeReflectiveActions, BeReflectiveVirtualProps, PropToAttrMapping} from './types';
+import {register} from "be-hive/register.js";
 
+export class BeReflectiveController implements BeReflectiveActions{
+    #target!: Element;
+    intro(proxy: Element & BeReflectiveVirtualProps, target: Element, beDecorProps: BeDecoratedProps){
+        this.#target = target;
+        const params = JSON.parse(proxy.getAttribute('is-' + beDecorProps.ifWantsToBe!)!) as {[key: string]: PropToAttrMapping};
+        const fullyQualifiedMap: {[key: string]: PropToAttrMapping} = {}
+        for(const propKey in params){
+            const propMap = params[propKey];
+            switch(typeof propMap){
+                case 'string':
+                    const reflectTo = propMap;
+                    fullyQualifiedMap[propKey] = {reflectTo};
+                    break;
+                case 'object':
+                    fullyQualifiedMap[propKey] = propMap;
+                    break;
+                default:
+                    throw 'NI';//Not implemented
+            }
+        }
+        proxy.map = fullyQualifiedMap;
+    }
 
-function doPoll(lastValue: any, key: string, self: any, prop: PropMonitor){
-    if(lastValue !== self[key]){
-        lastValue = self[key];
-        if(lastValue !== undefined && lastValue != null){
-            const sLV = lastValue.toString();
-            if(prop.as !== undefined){
-                self.setAttribute(prop.as, sLV)
+    onMap({map}: this){
+        for(const propKey in map){
+            const propMap = map[propKey];
+            const {reflectTo, maxDelay, once} = propMap;
+            const val = (<any>this.#target)[propKey];
+            if(val !== undefined){
+                this.setAttr(this.#target, val, reflectTo);
+                if(once) return;
+            }
+            if(maxDelay){
+                this.doPoll(propMap, propKey);
             }else{
-                self.dataset[key] = sLV;
+                let proto = this;
+                let prop: PropertyDescriptor | undefined = Object.getOwnPropertyDescriptor(proto, propKey);
+                while(proto && !prop){
+                    proto = Object.getPrototypeOf(proto);
+                    prop = Object.getOwnPropertyDescriptor(proto, propKey);
+                }
+                if(prop === undefined){
+                    throw {element: this, propKey, message: "Can't find property."};
+                }
+                const setter = prop.set!.bind(this);
+                const getter = prop.get!.bind(this);
+                Object.defineProperty(this, propKey, {
+                    get(){
+                        return getter();
+                    },
+                    set(nv){
+                        setter(nv);
+                        this.setAttr(this.#target, nv, reflectTo);
+                    },
+                    enumerable: true,
+                    configurable: true,
+                });     
             }
         }
     }
-    setTimeout(() => {
-        doPoll(lastValue, key, self, prop);
-    }, prop.poll);
+
+    doPoll(propMap: PropToAttrMapping, propKey: string){
+        const {maxDelay, once, reflectTo} = propMap;
+        setTimeout(() => {
+            const val = (<any>this.#target)[propKey];
+            if(val !== undefined){
+                this.setAttr(this.#target, val, reflectTo);
+                if(once) return;
+            }
+            this.doPoll(propMap, propKey);
+        }, maxDelay);
+    }
+
+    setAttr(self: Element, val: any, reflectTo: string){
+        if(reflectTo[0] === '.'){
+            const verb = val ? 'add' : 'remove';
+            self.classList[verb](reflectTo.slice(1));
+            
+        }else if(reflectTo.substr(0, 2) === '::'){
+            const verb = val ? 'add' : 'remove';
+            self.part[verb](reflectTo.slice(2));
+        }else{
+            const verb = val ? 'setAttribute' : 'removeAttribute';
+            self[verb](reflectTo, '');
+        }
+    }
 }
-const ce = new CE<XtalDecorCore<HTMLElement> & BeReflectiveProps>({
+
+export interface BeReflectiveController extends BeReflectiveProps{}
+
+const tagName = 'be-reflective';
+const ifWantsToBe = 'reflective';
+const upgrade = '*';
+
+define<BeReflectiveProps & BeDecoratedProps<BeReflectiveProps, BeReflectiveActions>, BeReflectiveActions>({
     config:{
-        tagName: 'be-reflective',
+        tagName,
         propDefaults:{
-            upgrade: '*',
-            ifWantsToBe: 'reflective',
-            props: {
-                src:{}
-            },
-            virtualProps:[],
+            virtualProps: ['map'],
+            noParse: true,
+            upgrade,
+            ifWantsToBe,
+            intro: 'intro',
+        },
+        actions: {
+            onMap: {
+                ifAllOf: ['map'],
+            }
         }
     },
     complexPropDefaults:{
-        actions:[
-            ({self, props, target}: any) => {
-                for(const key in props){
-                    const prop = props[key] as PropMonitor;
-                    if(prop.poll){
-                        let lastValue: any = undefined;
-                        doPoll(lastValue, key, self, prop);
-                    }else{
-                        const proto = target.constructor.prototype;
-                        const newProp = Object.getOwnPropertyDescriptor(proto, key)!;
-                        const setter = newProp.set!;
-                        const getter = newProp.get!;
-                        Object.defineProperty(target, key, {
-                            get(){
-                                const tempGetter = getter.bind(this);
-                                return tempGetter();
-                            },
-                            set(nv){
-                                if(nv !== null && nv !== undefined){
-                                    const sNV = nv.toString();
-                                    if(prop.as !== undefined){
-                                        self.setAttribute(prop.as, sNV);
-                                    }
-                                    self.dataset[key] = nv.toString();
-                                }
-
-                                const tempSetter = setter.bind(this);
-                                tempSetter(nv);
-                                
-                            },
-                            enumerable: true,
-                            configurable: true,
-                        });
-                    }
-
-                }
-            }
-        ],
-        init:(self: HTMLElement) => {
-            console.log('in init');
-        },
-        on:{},
-    },
-    superclass: XtalDecor,
+        controller: BeReflectiveController,
+    }
 });
 
-document.head.appendChild(document.createElement('be-reflective'));
+register(ifWantsToBe, upgrade, tagName);
+
+
